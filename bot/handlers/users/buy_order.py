64f8aca3c7e60c0507f488b1fff42a720c14b481
a.postnikov <a.postnikov@uzairports.com>
2023-11-16 18:38:34 +0500

    buy order 2

--- bot/handlers/users/buy_order.py
+++ bot/handlers/users/buy_order.py
@@ -4,7 +4,8 @@ from aiogram.dispatcher.filters import Regexp
 from aiogram.types import CallbackQuery, LabeledPrice, ContentType, PreCheckoutQuery, Message
 
 from data import config
-from loader import dp, _, bot
+from loader import dp, bot
+from models import OrderBuy
 
 
 async def process_pre_checkout_query(pre_checkout_query: PreCheckoutQuery):
@@ -16,6 +17,7 @@ async def process_successful_payment(message: Message):
     total_amount = message.successful_payment.total_amount
     id_order = message.successful_payment.invoice_payload
     phone_number = message.successful_payment.order_info.phone_number
+    name_client = message.successful_payment.order_info.name
     provider_payment = message.successful_payment.provider_payment_charge_id
 
     info_pay_user = (
@@ -24,11 +26,14 @@ async def process_successful_payment(message: Message):
             '\nНОМЕР ПЛАТЕЖА: ' + str(provider_payment) +
             '\nID КЛИЕНТА: ' + str(message.chat.id) +
             ('\nСУММА: {:,} ').format(total_amount / 100).replace(',', ' ') + str(currency) +
-            '\nТелефон пассажира: ' + str(phone_number) +
+            '\nТелефон клиента: ' + str(phone_number) +
             '\n\n   СПАСИБО ЗА ПОКУПКУ!'
     )
 
     await message.answer(text=info_pay_user)
+    
+    orderbuy = OrderBuy.update(user_name=name_client, phone_number=phone_number, payment_status=2, payment_number=provider_payment).where(OrderBuy.id == id_order)
+    orderbuy.execute()
 
     for admin_id in config.ADMINS:
         await bot.send_message(admin_id, info_pay_user)
@@ -38,29 +43,44 @@ async def process_successful_payment(message: Message):
 @dp.message_handler(process_successful_payment, content_types=ContentType.SUCCESSFUL_PAYMENT)
 
 
-@dp.callback_query_handler(Regexp('PAYMENT_SERVICE_'))
+@dp.callback_query_handler(Regexp('PAYMENT_ORDER_'))
 async def _change_payment_markup(callback_query: CallbackQuery):
 
-    await callback_query.message.edit_reply_markup(reply_markup=None)
+    code = callback_query.data[14:]
 
-    for serv in services:
-        title = serv.service
-        description = serv.info_reys
-        amount = (serv.total_sum) * 100
+    order = OrderBuy.select().where(OrderBuy.id==code)
+
+    res_list = ''
+    total = 0
+
+    for res in order:
+        for prod in res.products:
+            for ord in prod:
+                res_list = res_list + ord["title"] + '. Кол-во: ' + str(ord["qty"]) + '\n'
+                total = int(total) + int(ord["price"]) * int(ord["qty"])
+
+    for res in order:
+        address = res.address
+        res_list = res_list + 'Доставка: ' + address + '\n'
+        price_dostavka = int(res.address_price)
+
+    print(total, res_list, price_dostavka)
+
+    await callback_query.message.edit_reply_markup(reply_markup=None)
 
     currency = "rub"
     foto_url = 'https://i.pinimg.com/736x/5d/b1/7a/5db17ab1f8e0bad184cf9a879b46aaa3.jpg'
 
     PRICES = [
-        LabeledPrice(label='Заказ', amount=4200000),
-        LabeledPrice(label='Доставка', amount=30000)
+        LabeledPrice(label='Заказ', amount=total*100),
+        LabeledPrice(label='Доставка', amount=price_dostavka*100)
     ]
 
     await callback_query.bot.send_invoice(chat_id=callback_query.from_user.id,
-                                          title=title,
-                                          description=description,
-                                          payload=str(idService),
-                                          start_parameter='service-uzair-pay',
+                                          title='Оформление заказа',
+                                          description=res_list,
+                                          payload=str(code),
+                                          start_parameter='service-pizza-pay',
                                           currency=currency,
                                           prices=PRICES,
                                           photo_url=foto_url,
--- models/order.py
+++ models/order.py
@@ -9,22 +9,17 @@ from .base import BaseModel
 class OrderBuy(BaseModel):
     id = IdentityField(primary_key=True)
     user_id = BigIntegerField()
-<<<<<<< HEAD
     user_name = CharField(null=True)
     products = BinaryJSONField()
     price = IntegerField(default='ru')
     payment_status = IntegerField(default=1)
     phone_number = CharField(null=True)
-=======
-    products = BinaryJSONField()
-    price = IntegerField(default='ru')
-    payment_status = IntegerField(default=1)
->>>>>>> origin/master
     address = CharField(default=None)
     address_price = IntegerField(default=None)
     payment_number = CharField(default=None)
 
     created_at = DateTimeField(default=lambda: datetime.utcnow())
+
 
     def __repr__(self) -> str:
         return f'<User {self.username}>'
